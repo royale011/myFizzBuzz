6f159766a5713c6800c364094f10e3cfd9471418
Nero Lee <jxli@thoughtworks.com>
2017-01-11 00:12:32 +0800

    pass should_return_Fizz

--- src/main/java/FizzBuzzMaker/FizzBuzzMaker.java
+++ src/main/java/FizzBuzzMaker/FizzBuzzMaker.java
@@ -1,10 +1,15 @@
 package FizzBuzzMaker;
 
+import FizzBuzzChecker.FizzBuzzChecker;
+
 /**
  * Created by jxli on 11/01/2017.
  */
 public class FizzBuzzMaker {
   public static String makeString(int given) {
+    if (FizzBuzzChecker.isFizz(given)) {
+      return "Fizz";
+    }
     return String.valueOf(given);
   }
 }
